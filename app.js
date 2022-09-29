08e5b3a00ad36bb14228849abd6a6060606c7921
Frank South <frankstonesouth@gmail.com>
2022-09-29 12:16:37 -0400

    Added outline of necessary steps needed to achieve goal

--- app.js
+++ app.js
@@ -2,3 +2,15 @@
 
 
 //On refresh, event listener that plays the game
+
+
+
+//Dice roll random number
+const p1Roll = Math.floor(Math.random() * 6 + 1);
+const p2Roll = Math.floor(Math.random() * 6 + 1);
+
+
+//Update dice images
+
+
+//Update header & add flag image
