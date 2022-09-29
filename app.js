219a813017afe8312aa5d523f720b1b3880aaf47
Frank South <frankstonesouth@gmail.com>
2022-09-29 13:16:11 -0400

    Early iteration of script that rolls a number for each player and changes their dice images

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 //Query Selectors
+const playBtn = document.querySelector('.btn');
+const diceOne = document.querySelector('.diceOne');
+const diceTwo = document.querySelector('.diceTwo');
 
 
 //On refresh, event listener that plays the game
+playBtn.addEventListener('click', () => {
+    randomRoll();
+})
 
 
+//Dice roll for Player 1
+// const randomOne = () => {
+//     let p1Roll = Math.floor(Math.random() * 6 + 1);
+//     diceOne.setAttribute('src', `/images/dice${p1Roll}.png`);
+// }
 
-//Dice roll random number
-const p1Roll = Math.floor(Math.random() * 6 + 1);
-const p2Roll = Math.floor(Math.random() * 6 + 1);
+// //Dice roll for Player 2
+// const randomTwo = () => {
+//     let p2Roll = Math.floor(Math.random() * 6 + 1);
+//     diceTwo.setAttribute('src', `/images/dice${p2Roll}.png`);
+// }
+
+const randomRoll = () => {
+    //Player 1 Roll and image change
+    const p1Roll = Math.floor(Math.random() * 6 + 1);
+    diceOne.setAttribute('src', `/images/dice${p1Roll}.png`);
+    //Player 2 Roll and image change
+    const p2Roll = Math.floor(Math.random() * 6 + 1);
+    diceTwo.setAttribute('src', `/images/dice${p2Roll}.png`);
+}
 
 
 //Update dice images
+//setAttribute
+
 
 
 //Update header & add flag image
+//innerText / innerHTML
